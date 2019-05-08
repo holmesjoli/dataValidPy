8b4c3f6bf233e7d5667a57c480c1253a5a53761f
Joli Holmes <jh111@ad.rice.edu>
2019-05-08 14:10:21 -0500

    fixed typo in boolean functions that didn't convert them to dataframes

--- dataValidPy/tests/test_test_bool.py
+++ dataValidPy/tests/test_test_bool.py
@@ -2,7 +2,8 @@ import unittest
 import pandas as pd
 
 from dataValidPy.test_utils import test_pass, test_fail
-from dataValidPy.test_bool import test_all_true, test_all_false, test_any_true, test_any_false
+from dataValidPy.test_bool import test_all_true_class, test_all_false_class, \
+    test_any_true_class, test_any_false_class
 
 class BoolColsTestClass(unittest.TestCase):
 
@@ -14,21 +15,21 @@ class BoolColsTestClass(unittest.TestCase):
                                 "col2": [False, False],
                                 "col3": [True, False]})
 
-        self.tat1 = test_all_true(self.df.col1)
-        self.tat2 = test_all_true(self.df.col2)
-        self.tat3 = test_all_true(self.df.col3)
+        self.tat1 = test_all_true_class(self.df.col1)
+        self.tat2 = test_all_true_class(self.df.col2)
+        self.tat3 = test_all_true_class(self.df.col3)
 
-        self.taf1 = test_all_false(self.df.col1)
-        self.taf2 = test_all_false(self.df.col2)
-        self.taf3 = test_all_false(self.df.col3)
+        self.taf1 = test_all_false_class(self.df.col1)
+        self.taf2 = test_all_false_class(self.df.col2)
+        self.taf3 = test_all_false_class(self.df.col3)
 
-        self.tant1 = test_any_true(self.df.col1)
-        self.tant2 = test_any_true(self.df.col2)
-        self.tant3 = test_any_true(self.df.col3)
+        self.tant1 = test_any_true_class(self.df.col1)
+        self.tant2 = test_any_true_class(self.df.col2)
+        self.tant3 = test_any_true_class(self.df.col3)
 
-        self.tanf1 = test_any_false(self.df.col1)
-        self.tanf2 = test_any_false(self.df.col2)
-        self.tanf3 = test_any_false(self.df.col3)
+        self.tanf1 = test_any_false_class(self.df.col1)
+        self.tanf2 = test_any_false_class(self.df.col2)
+        self.tanf3 = test_any_false_class(self.df.col3)
 
     def test_test_all_true(self):
         self.assertTrue(hasattr(self.tat1, "td"))
